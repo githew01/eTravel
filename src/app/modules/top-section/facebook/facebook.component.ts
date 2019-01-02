0d075fe99381bc5d0b6f584247e539554d33576c
Harry He <Harry.He@innovapost.com>
2019-01-02 09:42:01 -0500

    top banner reponsive

--- src/app/modules/top-section/facebook/facebook.component.ts
+++ src/app/modules/top-section/facebook/facebook.component.ts
@@ -6,10 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./facebook.component.scss']
 })
 export class FacebookComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
