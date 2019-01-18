d43017651a257732337d60f763979bb0a222dcc1
Harry <githew01@users.noreply.github.com>
2019-01-17 23:02:56 -0500

    transaltion works

--- src/app/modules/top-section/top-section.module.ts
+++ src/app/modules/top-section/top-section.module.ts
@@ -1,12 +1,13 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { TopBannerComponent } from './top-banner/top-banner.component';
-import { FacebookComponent } from './facebook/facebook.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { TopBannerComponent } from "./top-banner/top-banner.component";
+import { FacebookComponent } from "./facebook/facebook.component";
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+import { SharedModule } from "../shared/shared.module";
 
 @NgModule({
   declarations: [TopBannerComponent, FacebookComponent],
-  imports: [CommonModule, FontAwesomeModule],
+  imports: [CommonModule, FontAwesomeModule, SharedModule],
   exports: [TopBannerComponent]
 })
 export class TopSectionModule {}
