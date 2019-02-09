c60cd2ce2088441f6810c412d69e5179b60a202b
Harry He <Harry.He@innovapost.com>
2019-02-09 14:50:33 -0500

    side nav and icon

--- src/app/modules/left-section/left-section.module.ts
+++ src/app/modules/left-section/left-section.module.ts
@@ -2,10 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
 import { LeftSectionComponent } from './left-section/left-section.component';
+import { MaterialModule } from '../shared/material.module';
+import { RouterModule } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { BrowserModule } from '@angular/platform-browser';
 
 @NgModule({
   declarations: [LeftSectionComponent],
-  imports: [CommonModule, SharedModule],
+  imports: [
+    CommonModule,
+    SharedModule,
+    MaterialModule,
+    RouterModule,
+    BrowserModule,
+    FontAwesomeModule
+  ],
   exports: [LeftSectionComponent]
 })
 export class LeftSectionModule {}
--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,10 +1,11 @@
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { TranslateModule } from "@ngx-translate/core";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, TranslateModule],
+  imports: [CommonModule, TranslateModule, BrowserAnimationsModule],
   exports: [CommonModule, TranslateModule]
 })
 export class SharedModule {}
